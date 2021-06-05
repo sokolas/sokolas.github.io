b053a350126d9754df57ea261a7905667bf754e2
Sokolas <sunrise.beast@gmail.com>
2021-06-05 11:53:33 +0300

    lambda test

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello'
+    },
+    methods: {
+        getClips: function() {
+            fetch("https://r3owimfazk.execute-api.us-east-2.amazonaws.com/default/getToken?code=qqqqq")
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                this.message = json;
+            })
+            .catch(err => console.log(err));
+        }
     }
 });
